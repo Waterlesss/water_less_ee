371c72a5a0e122c7624a8a72a2b7ba07872eaced
职回水 <1554333496@qq.com>
2022-06-28 18:12:27 +0800

    IO创建文件和删除文件

---
+++ src/IO/CreateTest.java
@@ -0,0 +1,43 @@
+package IO;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @Author: Waterless
+ * @Date: 2022/06/27/22:14
+ * @Description: File创建操作
+ */
+public class CreateTest {
+    public static void main(String[] args) throws IOException {
+        //1.绝对路径的方式创建
+        //1.1传入字符串的路径
+        File file1 = new File("C:\\Users\\86177\\Desktop\\show.png");
+        //1.2船夫父路径 + 子路径
+        File file2 = new File("C:\\Users\\86177\\Desktop","show.png");
+        //1.3以File传入parent
+        File parent = new File("C:\\Users\\86177\\Desktop");
+        File file3 = new File(parent,"show.png");
+        // 负数代表 file1 < file2
+        // 0 代表 file1 == file2
+        // 正数代表 file1 > file2
+        int cmp1 = file1.compareTo(file2);
+        int cmp2 = file2.compareTo(file3);
+        System.out.println(cmp1);
+        System.out.println(cmp2);
+
+        File file4 = new File("C:\\Users\\86177\\Desktop\\world.txt");
+        // 异常的知识点：Java 中的异常分为 受查异常（checked exception） or 非受查异常（uncheck exception）
+        // 方法中如果可能抛出受查异常，有语法要求
+        // 要么，我们自己 try catch，不再抛出受查异常
+        // 要么，在方法签名中通过 throws 声明有受查异常可能被抛出
+        // java.lang.RuntimeException 及其子类是非受查异常
+        // java.io.IOException 是受查异常
+        boolean r1 = file4.createNewFile();
+        System.out.println(r1);
+        // 这里打印 true
+        r1 = file4.createNewFile();
+        // 由于文件被创建过了，所以是 false
+        System.out.println(r1);
+    }
+}
---
+++ src/IO/DeleteTest.java
@@ -0,0 +1,33 @@
+package IO;
+
+import java.io.File;
+
+/**
+ * @Author: Waterless
+ * @Date: 2022/06/27/22:14
+ * @Description: File删除操作
+ */
+public class DeleteTest {
+    public static void main(String[] args) throws Exception {
+        File file1 = new File("C:\\Users\\86177\\Desktop");
+//        System.out.println(file1.delete());
+        traversal(file1);
+    }
+    private static void traversal(File dir) throws Exception {
+        File[] files = dir.listFiles();
+        // 查看这个目录下的所有孩子（不是子孙）
+        for (File file : files) {
+            if (file.isDirectory()) {
+                // 如果这个孩子也是目录，继续深度优先进行遍历
+                //getCanonicalPath() 返回file对象修饰过的绝对路径
+                System.out.println(file.getCanonicalPath() + "\\");
+                traversal(file);
+                // 当深度优先的遍历完成时，则可以认为该目录一定是空目录了
+//                file.delete();
+            } else {
+                System.out.println(file.getCanonicalPath());
+                // 得到这个文件的一个标准（去除一切无意义的 . 和 ..)
+//                file.delete();
+            }
+        }
+    }}
